a610f154a1182809c44a3bb265246e30b71dcff0
0Petrovich <korchemkin.igor@gmail.com>
2021-12-20 00:48:36 +0500

    Напишите программу, принимающую на вход целое число, которая выводит True, если переданное значение попадает в интервал (-15, 12] \cup (14, 17) \cup [19, +\infty)(−15,12]∪(14,17)∪[19,+∞) и False в противном случае (регистр символов имеет значение).
    Обратите внимание на разные скобки, используемые для обозначения интервалов. В задании используются полуоткрытые и открытые интервалы. Подробнее про это вы можете прочитать, например, на википедии (полуинтервал, промежуток).

---
+++ PetrovichIgor/1-я Неделя/1.5_2 формула_ошибка.py
@@ -0,0 +1,6 @@
+42/(4+2*(-2))
+'''ZeroDivisionError                         Traceback (most recent call last)
+<ipython-input-3-1cc156a7f7e2> in <module>()
+----> 1 42/(4+2*(-2))
+
+ZeroDivisionError: division by zero'''
---
+++ PetrovichIgor/1-я Неделя/1.9_2(3) логические значениея.py
@@ -0,0 +1,8 @@
+x = 5
+y = 10
+print(y > x * x or y >= 2 * x and x < y)
+# True
+a = True
+b = False
+print(a and b or not a and not b)
+# False
