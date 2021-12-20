d79846b7146980b73c2a4c36b6998d2740e158ce
0Petrovich <korchemkin.igor@gmail.com>
2021-12-20 14:56:56 +0500

    Когда Павел учился в школе, он запоминал таблицу умножения прямоугольными блоками. Для тренировок ему бы очень пригодилась программа, которая показывала бы блок таблицы умножения.
    Напишите программу, на вход которой даются четыре числа a, b, c и d, каждое в своей строке. Программа должна вывести фрагмент таблицы умножения для всех чисел отрезка [a; b][a;b] на все числа отрезка [c;d][c;d].
    
    Числа a, b, c и d являются натуральными и не превосходят 10, a≤b, c≤d.
    
    Следуйте формату вывода из примера, для разделения элементов внутри строки используйте '\t' — символ табуляции. Заметьте, что левым столбцом и верхней строкой выводятся сами числа из заданных отрезков — заголовочные столбец и строка таблицы.

---
+++ PetrovichIgor/2-я Неделя/2.3_1 for.py
@@ -0,0 +1,12 @@
+c,d,a,b= (int(input())for _ in 'abcd')# put your python code here
+x=a
+while x<=b:
+      print('\t',x,end='')
+      x+=1
+for c in range(c,d+1):
+    print()
+    print(c,'\t',end='')
+    y=a
+    while y<=b:
+        print(y*c,'\t',end='')
+        y+=1
