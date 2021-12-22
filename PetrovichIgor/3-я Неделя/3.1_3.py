65de278724bbc060314502143ab6be732e872bb6
0Petrovich <korchemkin.igor@gmail.com>
2021-12-22 15:59:09 +0500

    Напишите функцию modify_list(l), которая принимает на вход список целых чисел, удаляет из него все нечётные значения, а чётные нацело делит на два. Функция не должна ничего возвращать, требуется только изменение переданного списка, например:
    lst = [1, 2, 3, 4, 5, 6]
    print(modify_list(lst))  # None
    print(lst)               # [1, 2, 3]
    modify_list(lst)
    print(lst)               # [1]
    
    lst = [10, 5, 8, 3]
    modify_list(lst)
    print(lst)               # [5, 4]
    Функция не должна осуществлять ввод/вывод информации.

--- PetrovichIgor/3-я Неделя/3.1_3.py
+++ PetrovichIgor/3-я Неделя/3.1_3.py
@@ -1,17 +1,17 @@
-lst = [-2, 2, 3, 3, 5, -0, 4, 5, 7, 6]
+lst = [-2, 2, 3, 3, 5, 4, 5, 7, 6] # проверочная строка
 #print(modify_list(lst))  # None
 #print(lst)
-def modify_list(l):# put your python code here
+def modify_list(l):# # put your python code here
     l=lst#[int(i) for i in input().split()]
-    d=len(lst)
+    d=len(l)
     i=0
     for j in range(d):#while j<=d:
         j-=i
         #print(lst[j])
-        if lst[j]%2==0: lst.append(int(lst[j]/2));lst.pop(j);i+=1
+        if l[j]%2==0: l.append(int(l[j]/2));l.pop(j);i+=1
         else:
-            lst.pop(j);i+=1
-    return lst
+            l.pop(j);i+=1
+    return l
 #lst = [1, 2, 3, 4, 5, 6]
 print(modify_list(lst))
 print(modify_list(lst))  # None
