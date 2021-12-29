680ee37a411771ca19572b9a7280babfd66f9a91
Asder <asderof@gmail.com>
2021-12-29 18:15:23 +0500

    test

--- OlegKotelnikov/6.py
+++ OlegKotelnikov/6.py
@@ -1,12 +1,13 @@
-if n == 0:
-    v[n][i] += c[(len(c) - 1)][i]
-elif n == len(c) - 1:
-    v[n][i] += c[0][i]
-else:
-    v[n][i] += c[n - 1][i] + c[n + 1][i]
-if i == 0:
-    v[n][i] += c[n][len(c[0]) - 1]
-elif i == len(c) - 1:
-    v[n][i] += c[n][0]
-else:
-    v[n][i] += c[n][i - 1] + c[n][i + 1]
+import keyword
+print('список ключевых слов')
+for i in range(len(keyword.kwlist)):
+    e = '\n' if i % 5 == 4 else ' / '
+    print(keyword.kwlist[i], end=e)
+print()
+print('*--*--'*25)
+print()
+import builtins
+print('список встроенных идентификаторов')
+for i in range(len(dir(builtins))):
+    e = '\n' if i % 5 == 4 else ' / '
+    print((dir(builtins)[i]), end=e)
