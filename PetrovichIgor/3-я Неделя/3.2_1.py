c5aeb217e3d69c27dcf27dd04d712676c55d3351
0Petrovich <korchemkin.igor@gmail.com>
2021-12-29 13:01:11 +0500

    Второй день работы

--- PetrovichIgor/3-я Неделя/3.2_1.py
+++ PetrovichIgor/3-я Неделя/3.2_1.py
@@ -1,4 +1,22 @@
+d = {}
 def update_dictionary(d, key, value):
-  if d.get(key)!=None:
-    d[key] = value
-  d[2*key] = value
+  #d, key, value = {}, 1, -1
+  if d.get(key) != None:
+    d[key] += [value]; print(d)#; return
+    return
+  b=key*2
+  if d.get(b) == None:
+    d[b] = [value]#; print(d)
+
+    return #d[key*2].append(value)
+  d[b].append(value)#d[b] = [value]
+  return
+
+if __name__ == '__main__':
+  print('PyCharm')
+  print(update_dictionary(d, 1, -1))
+  print(d)
+  update_dictionary(d, 2, -2)
+  print(d)
+  update_dictionary(d, 1, -3)
+  print(d)
