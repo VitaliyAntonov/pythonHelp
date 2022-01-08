fbdb23af0c327d04aeded9b27e0147ddf042bfa5
0Petrovich <korchemkin.igor@gmail.com>
2022-01-08 20:53:40 +0500

    На прошлой неделе мы сжимали строки, используя кодирование повторов. Теперь нашей задачей будет восстановление исходной строки обратно.
    Напишите программу, которая считывает из файла строку, соответствующую тексту, сжатому с помощью кодирования повторов, и производит обратную операцию, получая исходный текст.
    
    Запишите полученный текст в файл и прикрепите его, как ответ на это задание.
    
    В исходном тексте не встречаются цифры, так что код однозначно интерпретируем.
    
    Примечание. Это первое задание типа Dataset Quiz.В таких заданиях после нажатия "Start Quiz" у вас появляется ссылка "download your dataset". Используйте эту ссылку для того, чтобы загрузить файл со входными данными к себе на компьютер. Запустите вашу программу, используя этот файл в качестве входных данных. Выходной файл, который при этом у вас получится, надо отправить в качестве ответа на эту задачу.

--- PetrovichIgor/3-я Неделя/3.4_1 Ввод-Вывод.py
+++ PetrovichIgor/3-я Неделя/3.4_1 Ввод-Вывод.py
@@ -9,21 +9,21 @@
 s = open("/home/petrovich/Soft/pycharm-community-2021.3/TiachDonload/Prover002", "r").readline()
 #with open('/home/petrovich/Soft/pycharm-community-2021.3/TiachDonload/dataset_3363_2.txt') as inf:
 #     s = inf.readline()
-y, tmpOut, x = str(0), str(), str()
-for i in s: #hf: #(::-1):
-    if '0' <= i <= '9':
-        y += i
-        continue
-    tmpOut += x * int(y)
-    x = i
-    y = str(0)
-tmpOut += x * int(y)
+y, tmpOut, x = str(0), str(), str() # Создание пустых строк
+for i in s: # Перебор всех символов строки "s" по порядку
+    if '0' <= i <= '9': # Проверка очередного символа на число
+        y += i # Если число то сливается с "y='0'+'i'"
+        continue # Переход к следущему символу
+    tmpOut += x * int(y) # Запись в файл символа "х" "у" раз (в первый цикл пустая строка 0 раз)
+    x = i # Запоминание не цифры
+    y = str(0) # Обнуление индекса не цифры
+tmpOut += x * int(y) # Запись в файл последнего символа "х" "у" раз
 #print(tmpOut)
 #shf.close()
 #rez = open('home','petrovich','Soft','pycharm-community-2021.3','TiachDonload','rez.txt','w')
-with open('/home/petrovich/Soft/pycharm-community-2021.3/TiachDonload/rez.txt','w') as rez:
-    rez.write(tmpOut)
+with open('/home/petrovich/Soft/pycharm-community-2021.3/TiachDonload/rez.txt','w') as rez: # Открытие файла для записи
+    rez.write(tmpOut) # Запись строки в файл
     #rez = open(os.path.join('home','petrovich','Soft','ycharm-community-2021.3','TiachDonload','rez.txt'),'w') # Открытие файла для записи из каталога и подкаталога.
     #rez.write(tmpOut)
-print(tmpOut) #, end='')
+print(tmpOut) # Проверочная строка результата
 #rez.close()
