1799666af43c63f845ca5729267173283b1ee144
0Petrovich <korchemkin.igor@gmail.com>
2022-01-12 18:09:56 +0500

    Имеется файл с данными по успеваемости абитуриентов. Он представляет из себя набор строк, где в каждой строке записана следующая информация:
    Фамилия;Оценка_по_математике;Оценка_по_физике;Оценка_по_русскому_языку
    
    Поля внутри строки разделены точкой с запятой, оценки — целые числа.
    
    Напишите программу, которая считывает исходный файл с подобной структурой и для каждого абитуриента записывает его среднюю оценку по трём предметам на отдельной строке, соответствующей этому абитуриенту, в файл с ответом.
    
    Также вычислите средние баллы по математике, физике и русскому языку по всем абитуриентам и добавьте полученные значения, разделённые пробелом, последней строкой в файл с ответом.
    
    В качестве ответа на задание прикрепите полученный файл со средними оценками по каждому ученику и одной строкой со средними оценками по трём предметам.
    
    Примечание. Для разбиения строки на части по символу ';' можно использовать метод split следующим образом:
    
    print('First;Second-1 Second-2;Third'.split(';'))
    # ['First', 'Second-1 Second-2', 'Third']
    Sample Input:
    
    Петров;85;92;78
    Сидоров;100;88;94
    Иванов;58;72;85
    Sample Output:
    
    85.0
    94.0
    71.666666667
    81.0 84.0 85.666666667

---
+++ PetrovichIgor/3-я Неделя/3.4_3 Средний балл студентов.py
@@ -0,0 +1,27 @@
+inf = open('/home/petrovich/Downloads/dataset_3363_4 (8).txt')
+#inf = open('/home/petrovich/Soft/pycharm-community-2021.3/TiachDonload/Prover003.txt')
+s = inf.readline().strip().split(';')  # ['Youmans', '59', '83', '45']
+#s = inf.readlines()# ['Youmans;59;83;45\n', 'Larkins;55;44;31\n', 'Lawman;24;33;32\n',..., 'Bradberry;46;99;39\n']
+lst, i, mat, fiz, rus = [], 0, 0, 0, 0
+while s != ['']:
+    #print('s=', s)
+    #lst += s
+    #print('lst=', lst)
+    sri = round(((float(s[1])+float(s[2])+float(s[3]))/3), 9)
+
+    with open('/home/petrovich/Soft/pycharm-community-2021.3/TiachDonload/rez3_4_3 (8).txt', 'a') as rez:
+        rez.write(str(sri)+'\n')# (round(sri),9)
+    #print(sri)
+    mat += float(s[1])
+    fiz += float(s[2])
+    rus += float(s[3])
+    i += 1
+    #print(mat, fiz, rus, i)
+    s = inf.readline().strip().split(';')  # ['Youmans', '59', '83', '45']
+#print(mat/i, fiz/i, rus/i)
+srm = round((mat/i), 9)
+srf = round((fiz/i), 9)
+srr = round((rus/i), 9)
+#print(srm, srf, srr)
+with open('/home/petrovich/Soft/pycharm-community-2021.3/TiachDonload/rez3_4_3 (8).txt', 'a') as rez:
+    rez.write(str(round((mat/i), 9))+' '+str(round((fiz/i), 9))+' '+str(round((rus/i), 9))+'\n')
