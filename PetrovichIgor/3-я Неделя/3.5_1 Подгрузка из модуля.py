f47767a29180b2b1777cc3e888c360ffc5b5c172
0Petrovich <korchemkin.igor@gmail.com>
2022-01-13 14:40:33 +0500

    Напишите программу, которая подключает модуль math и, используя значение числа \piπ из этого модуля, находит для переданного ей на стандартный ввод радиуса круга периметр этого круга и выводит его на стандартный вывод.

--- PetrovichIgor/3-я Неделя/3.5_1 Подгрузка из модуля.py
+++ PetrovichIgor/3-я Неделя/3.5_1 Подгрузка из модуля.py
@@ -1,3 +1,3 @@
-from math import pi
-r = float(input())
-print(2*pi*r)
+from math import pi as Пи# Подгрузка значения pi из модуля math, с одновременным переименованием в Пи
+r = float(input('Введите значение радиуса'))
+print(2*Пи*r)# Вывод переиметра круга
