a68eb999b8f342325709c431f31ab3b9cf77f7e2
0Petrovich <korchemkin.igor@gmail.com>
2022-01-13 21:42:40 +0500

    Скачайте файл. В нём указан адрес другого файла, который нужно скачать с использованием модуля requests и посчитать число строк в нём.
    Используйте функцию get для получения файла (имеет смысл вызвать метод strip к передаваемому параметру, чтобы убрать пробельные символы по краям).
    
    После получения файла вы можете проверить результат, обратившись к полю text. Если результат работы скрипта не принимается, проверьте поле url на правильность. Для подсчёта количества строк разбейте текст с помощью метода splitlines.
    
    В поле ответа введите одно число или отправьте файл, содержащий одно число.

---
+++ PetrovichIgor/3-я Неделя/3.6_2 Подсчёт строк в файле открытом по ссылке из файла.py
@@ -0,0 +1,15 @@
+from requests import get
+#inf = open('/home/petrovich/Downloads/dataset.txt')
+#inf = open('/home/petrovich/Soft/pycharm-community-2021.3/TiachDonload/Prover004.txt')
+with open("/home/petrovich/Downloads/dataset_3378_2 (1).txt", "r") as inf:
+    line = inf.readline().strip()# Строка из файла
+    print('line-', line)
+    obl = get(line) # Запрос по строке из файла
+    print('obl.text', obl.text) # Печать ответа на запрос в виде текста
+    rez = len(obl.text.splitlines())
+    print('obl-', obl)
+    #print('obl.splitlines()', obl.splitlines())
+    print('obl.text.splitlines()-', obl.text.splitlines())
+    print(rez)
+    '''with open("dataset.txt", "r") as inf:
+        line = inf.readline().strip()'''
