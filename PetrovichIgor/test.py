5ea86632d7d0b60f1fdda1dbaba6ae5dd913de76
0Petrovich <korchemkin.igor@gmail.com>
2022-01-14 16:41:16 +0500

    Дополнение теста для наглядности

--- PetrovichIgor/test.py
+++ PetrovichIgor/test.py
@@ -3,7 +3,8 @@ print('type(True)', type(True))
 print('''type('False')''', type('False'))
 print('type([str])', type([str]))
 print('type(1.0)', type(1.0))
-print(0.3+0.3+0.3 == 0.9)
+print(0.3+0.3+0.3 == 0.9) #False
+print(0.3+0.3+0.3) #0.8999999999999999
 n, i, z = 6, 2, 0
 for i in range(n):
     while i<=4:
@@ -13,11 +14,12 @@ for i in range(n):
         i-=1
         break
     continue
-print(i, z)
+print('i=', i,'итераций в цикле пока (while)', z)
 
-A=int(input())
-B=int(input())
-H=int(input())# put your python code here
+print('Вводимые числа должны быть целыми')
+A=int(input('Рекомендованое время сна, введите'))
+B=int(input('Максимально допустимое время сна, введите'))
+H=int(input('Время сна, введите'))# put your python code here
 if A<=H<=B:
     print('Это нормально')
 if H<A:
