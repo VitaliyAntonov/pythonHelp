e98ff5a6959b385926810f5dc2a8a2496b48ada3
Asder <asderof@gmail.com>
2022-01-26 20:18:11 +0500

    крестики нолики. тупой ИИ

--- OlegKotelnikov/x_and_o.py
+++ OlegKotelnikov/x_and_o.py
@@ -1,3 +1,5 @@
+import random
+
 t = [[0 for x in range(14)] for y in range(8)]
 t[0] = [' ', ' ', ' ', 'A', ' ', ' ', ' ', 'B', ' ', ' ', ' ', 'C', ' ', ' ']
 t[1] = [' ', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-']
@@ -10,22 +12,23 @@ t[7] = [' ', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-']
 n = 9
 h = 0
 winner = ''
-
+valid_t = [[2, 3],[2, 7],[2, 11],[4, 3],[4, 11],[4, 7],[6, 3],[6, 7],[6, 11]]
+nonvalid_t = []
 def calc_table(x = '0', y = '0', s = ''):
 
-    if x == 'A' or x == 'a':
+    if x == 'A' or x == 'a' or x == 3:
         x = 3
-    elif x == 'B' or x == 'b':
+    elif x == 'B' or x == 'b' or x == 7:
         x = 7
-    elif x == 'C' or x == 'c':
+    elif x == 'C' or x == 'c' or x == 11:
         x = 11
     else:
         return print('координата X - A, B или С (можно a b c)')
-    if y == '1':
+    if y == '1' or y == 2:
         y = 2
-    elif y == '2':
+    elif y == '2' or y == 4:
         y = 4
-    elif y == '3':
+    elif y == '3' or y == 6:
         y = 6
     else:
         return print('координата Y - 1, 2 или 3')
@@ -37,10 +40,19 @@ def calc_table(x = '0', y = '0', s = ''):
         t[y][x] = s
     else:
         return print('в тоже место ставить нельзя')
+    nonvalid_t.append([y ,x, s])
+    valid_t.remove([y, x])
     print_table()
     win(s)
+    if winner != '':
+        print(f'Поздравляю {winner} с победой!')
+        return exit()
     global h
     h += 1
+
+def good_move(s):
+    a = random.choice(valid_t)
+    calc_table(a[1], a[0], s)
 
 def print_table():
     for i in range(8):
@@ -62,18 +74,39 @@ def win(s):
         winner = s
         return
 
-
+print('Первый игрок (крестики):')
+print('Человек  - 1')
+print('Тупой ИИ - 2\n')
+while True:
+    player1 = input('введите 1 или 2 - ')
+    if player1 == '1' or player1 == '2':
+        if player1 == '1':
+            player1 = 0
+        break
+print()
+print('Второй игрок (нолики):')
+print('Человек  - 1')
+print('Тупой ИИ - 2\n')
+while True:
+    player2 = input('введите 1 или 2 - ')
+    if player2 == '1' or player2 == '2':
+        if player2 == '1':
+            player2 = 1
+        break
+print()
 print('Пример ввода координат - a 1')
 print_table()
 while h < n:
-    a = input('x y - ').split(' ')
-    try:
-        calc_table(a[0],a[1],h%2)
-    except BaseException:
-        print('ошибка ввода')
-        continue
-    if winner != '':
-        print(f'Поздравляю {winner} с победой!')
-        break
+    if player1 == h % 2 or player2 == h % 2:
+        a = input('x y - ').split(' ')
+        try:
+            calc_table(a[0], a[1], h % 2)
+        except BaseException:
+            print('ошибка ввода')
+            continue
+    if player1 == '2' and h % 2 == 0:
+        good_move(0)
+    if player2 == '2' and h % 2 == 1 and h < n - 1:
+        good_move(1)
 else:
     print('Ничья')
