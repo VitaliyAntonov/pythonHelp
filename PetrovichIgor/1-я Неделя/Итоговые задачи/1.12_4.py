61f6b6594dfd37efd21eb7cad4a6c397d99b0402
0Petrovich <korchemkin.igor@gmail.com>
2022-03-16 17:26:20 +0500

    переименование файлов

--- PetrovichIgor/1-я Неделя/Итоговые задачи/1.12_4.py
+++
@@ -1,4 +0,0 @@
-P= input()
-if P=='треугольник':[[print((p * (p - a) * (p - b) * (p - c))**0.5) for p in [(a + b + c) / 2]] for a, b, c in [[int(input()) for _ in 'abc']]]
-elif P=='прямоугольник':[print(a*b) for a, b in [[int(input()) for _ in 'ab']]]
-elif P=='круг':print(3.14*float(input())**2)
